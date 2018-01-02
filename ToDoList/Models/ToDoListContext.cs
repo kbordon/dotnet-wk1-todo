19cc5e5c985cb075eb689ece06352c8ba9d8bfa0
Kimberly Bordon and Rakhee Gandhi <kimberly+rakhee@>
2018-01-02 14:50:04 -0800

    create todolist following lesson plan

---
+++ ToDoList/Models/ToDoListContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
+{
+	public class ToDoListContext : DbContext
+	{
+		public virtual DbSet<Category> Categories { get; set; }
+		public virtual DbSet<Item> Items { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		=> optionsBuilder
+				 .UseMySql(@"Server=localhost;Port=8889;database=todolist;uid=root;pwd=root;");
+	}
+}
