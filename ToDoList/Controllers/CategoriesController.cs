24e1cb6b4e3d1af17e5e0da04d389b496c20374c
rpgandhi <rpgandhi@gmail.com>
2018-01-02 14:41:52 -0800

    * .gitignore: Create todolist following lessons
    * ToDoList.sln:
    * Startup.cs:
    * Program.cs:
    * Item.cs:
    * ToDoList.csproj:
    * Category.cs:
    * Edit.cshtml:
    * Index.cshtml:
    * Index.cshtml:
    * Delete.cshtml:
    * Create.cshtml:
    * Details.cshtml:
    * _Layout.cshtml:
    * Edit.cshtml:
    * Index.cshtml:
    * HomeController.cs:
    * ItemsController.cs:
    * Create.cshtml:
    * Delete.cshtml:
    * Details.cshtml:
    * CategoriesController.cs:

---
+++ ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoList.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        public IActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisCategory = db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisCategory = db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            db.Entry(category).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var thisCategory = db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisCategory = db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            db.Categories.Remove(thisCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList.Models
+{
+    [Table("Items")]
+    public class Item
+    {
+        [Key]
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+		public int CategoryId { get; set; }
+		public virtual Category Category { get; set; }
+
+        public Item()
+        {
+        }
+    }
+}
